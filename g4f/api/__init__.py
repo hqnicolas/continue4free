231e759fe8744392971a0a1840fe1d031441ff66
nicolas <hqnicolas@msn.com>
2024-11-13 22:35:04 -0300

    Merge pull request #2331 from NoelisTired/main

--- g4f/api/__init__.py
+++ g4f/api/__init__.py
@@ -193,13 +193,9 @@ class Api:
                     return JSONResponse(response_list[0].to_json())
 
                 # Streaming response
-                async def async_generator(sync_gen):
-                    for item in sync_gen:
-                        yield item
-
                 async def streaming():
                     try:
-                        async for chunk in async_generator(response):
+                        async for chunk in response:
                             yield f"data: {json.dumps(chunk.to_json())}\n\n"
                     except GeneratorExit:
                         pass
